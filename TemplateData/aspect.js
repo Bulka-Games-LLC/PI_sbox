193d0363d0b33230ce24faa0c7ce2ae7d90421c5
Aleksandr Polianskii <drsage.pro@gmail.com>
2026-07-01 18:38:57 +0200

    341

--- TemplateData/aspect.js
+++ TemplateData/aspect.js
@@ -1,41 +1,21 @@
 (function () {
-  const ASPECT = 16 / 9;
-  const HEIGHT_RATIO = 0.85;
-  const WIDTH_RATIO = 0.9;
+  const MIN_ASPECT = 16 / 9;
 
- function setRootHeight() {
-	console.log("setRootHeight");
+  function resizeUnity() {
+    const container = document.getElementById("unity-container");
+    if (!container) return;
 
-	const h0 = window.innerHeight;
-	document.getElementById("main").style.height = h0 + "px";
+    let w = window.innerWidth;
+    let h = window.innerHeight;
 
-    	const container = document.getElementById("webglcontent");
-    	if (!container) return;
+    if (w / h < MIN_ASPECT) {
+      h = w / MIN_ASPECT;
+    }
 
-    	const maxW = window.innerWidth * WIDTH_RATIO;
-    	const baseH = window.innerHeight * HEIGHT_RATIO;
+    container.style.width = w + "px";
+    container.style.height = h + "px";
+  }
 
-    	let h = baseH;
-    	let w = h * ASPECT;
-
-    	if (w > maxW) {
-      		w = maxW;
-      		h = w / ASPECT;
-    		}
-
-    	container.style.width = w + "px";
-    	container.style.height = h + "px";
-    	container.style.margin = "0 auto";
-  	}
-
-function refreshNutakuFrame(){
-	console.log("refreshNutakuFrame");
-	NutakuGI.setIframeSize();
-	}
-
-window.setRootHeight = setRootHeight;
-window.refreshNutakuFrame = refreshNutakuFrame;
-document.addEventListener("fullscreenchange", refreshNutakuFrame);
-window.addEventListener("resize", setRootHeight);
-window.addEventListener("load", setRootHeight);
+  window.addEventListener("resize", resizeUnity);
+  window.addEventListener("load", resizeUnity);
 })();
